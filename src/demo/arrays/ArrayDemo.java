de3952df4084f06f0ef3a688d2a1c05e7f4bfed4
Ryan Desmond <ryan@codingnomads.co>
2020-12-08 08:53:22 -0800

    simple updates to formatting

--- src/demo/arrays/ArrayDemo.java
+++ src/demo/arrays/ArrayDemo.java
@@ -1,4 +1,24 @@
 package demo.arrays;
 
+/**
+ * Created by ryandesmond - https://codingnomads.co
+ */
+
 public class ArrayDemo {
+    public static void main(String[] args) {
+        /* -----------------------------------------
+                Simple Array
+        ----------------------------------------- */
+
+
+        /* -----------------------------------------
+                2-Dimension Array
+        ----------------------------------------- */
+
+
+        /* -----------------------------------------
+                For-Each Loop
+        ----------------------------------------- */
+    }
+
 }
--- src/demo/conditionals/if_else/IfElseDemo.java
+++ src/demo/conditionals/if_else/IfElseDemo.java
@@ -1,5 +1,9 @@
 package demo.conditionals.if_else;
 
+/**
+ * Created by ryandesmond - https://codingnomads.co
+ */
+
 public class IfElseDemo {
 
     public static void main(String[] args) {
